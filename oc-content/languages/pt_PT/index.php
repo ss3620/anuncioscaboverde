a7a0b16accc89134805e1527a441cb2bdd911069
kshailendrasingh2 <kshailendrasingh2@gmail.com>
2026-07-26 06:55:52 +0530

    fix language

---
+++ oc-content/languages/pt_PT/index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Portuguese (Cabo Verde) locale info for Osclass.
+ * Reuses European Portuguese (pt_PT) but is presented as "Português (Cabo Verde)".
+ */
+
+function locale_pt_PT_info() {
+  return array(
+    'name'            => 'Português (Cabo Verde)',
+    'short_name'      => 'Português',
+    'description'     => 'European Portuguese translation adapted for Cabo Verde',
+    'version'         => '8.3.1',
+    'author_name'     => 'Anuncios Cabo Verde',
+    'author_url'      => 'https://www.anuncioscaboverde.com/',
+    'currency_format' => '{NUMBER} {CURRENCY}',
+    'date_format'     => 'd/m/Y',
+    'stop_words'      => 'a,ao,aos,as,com,como,da,das,de,do,dos,e,em,na,nas,no,nos,o,os,ou,para,por,que,se,um,uma,uns,umas,the'
+  );
+}
+
+?>
